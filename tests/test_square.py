aadb02890dce4e2381a93bc12c0eb523bd2cd482
Kudryavtseva Natalia <calatik11226@gmail.com>
2024-11-18 01:27:14 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,52 +1,46 @@
 import unittest
+import math
 from square import area, perimeter
 
 
 class TestSquareFunctions(unittest.TestCase):
 
     def test_area_valid(self):
+        self.assertAlmostEqual(area(1), 1)
+        self.assertAlmostEqual(area(2), 4)
 
-        self.assertEqual(area(1), 1)
-        self.assertEqual(area(2), 4)
-        self.assertEqual(area(5), 25)
-
-    def test_area_zero_vaiues(self):
+    def test_area_zero_values(self):
         with self.assertRaises(ValueError):
             area(0)
 
-    def test_area_negative_vaiues(self):
+    def test_area_negative_values(self):
         with self.assertRaises(ValueError):
             area(-1)
 
     def test_area_non_number(self):
-	with self.assertRaises(ValueError):
-            area("abc")
-
-	with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
+            area("3")
+        with self.assertRaises(TypeError):
             area(None)
 
     def test_perimeter_valid(self):
+        self.assertAlmostEqual(perimeter(1), 4)
+        self.assertAlmostEqual(perimeter(2), 8)
 
-        self.assertEqual(perimeter(1), 4)
-        self.assertEqual(perimeter(2), 8)
-        self.assertEqual(perimeter(5), 20)
-
-    def test_perimeter_zero_vaiues(self):
+    def test_perimeter_zero_values(self):
         with self.assertRaises(ValueError):
             perimeter(0)
 
-    def test_perimeter_negative_vaiues(self):
+    def test_perimeter_negative_values(self):
         with self.assertRaises(ValueError):
             perimeter(-1)
 
     def test_perimeter_non_number(self):
-	with self.assertRaises(ValueError):
-            perimeter("abc")
-
-	with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
+            perimeter("3")
+        with self.assertRaises(TypeError):
             perimeter(None)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
