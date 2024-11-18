feb710e87395af195ab6851a33aa574135e9c102
Kudryavtseva Natalia <calatik11226@gmail.com>
2024-11-18 13:12:47 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,10 +1,9 @@
 import unittest
-import math
 from calculate import calc
 
 
 class TestCalcFunction(unittest.TestCase):
-    
+
     def test_invalid_figure(self):
         with self.assertRaises(AssertionError):
             calc('hexagon', 'area', [5])
@@ -16,6 +15,7 @@ class TestCalcFunction(unittest.TestCase):
     def test_invalid_sizes(self):
         with self.assertRaises(TypeError):
             calc('circle', 'area', [3, 'a', 5])
+
 
 if __name__ == "__main__":
     unittest.main()
